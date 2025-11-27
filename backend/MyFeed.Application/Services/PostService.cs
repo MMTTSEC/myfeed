49cd39784c75bc024f0c5cc5b719ff5b07f6f788
MMTTSEC <mohammed.thabit.syne24lin@edu.tucsweden.se>
2025-11-27 13:24:41 +0100

    Create initial backend folder structure

---
+++ backend/MyFeed.Application/Services/PostService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFeed.Application.Services
+{
+    internal class PostService
+    {
+    }
+}
