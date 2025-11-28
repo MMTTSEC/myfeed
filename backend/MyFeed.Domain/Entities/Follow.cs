7ab8d1bcdb714489251293e85842b177f86d4b9c
MMTTSEC <mohammed.thabit.syne24lin@edu.tucsweden.se>
2025-11-28 20:32:20 +0100

    create follow entity

---
+++ backend/MyFeed.Domain/Entities/Follow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFeed.Domain.Entities
+{
+    public class Follow
+    {
+        public int FollowerId { get; }
+        public int FolloweeId { get; }
+        public DateTime CreatedAt { get; }
+
+        public Follow(int followerId, int followeeId)
+        {
+            FollowerId = followerId;
+            FolloweeId = followeeId;
+            CreatedAt = DateTime.UtcNow;
+        }
+
+    }
+}
---
+++ backend/MyFeed.Tests/Domain/FollowTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using MyFeed.Domain.Entities;
+
+namespace MyFeed.Tests.Domain
+{
+    public class FollowTests
+    {
+        [Fact]
+        public void FollowingUser_CreatesFollowSuccessfully()
+        {
+            // Arrange
+            int followerUserId = 1;  
+            int followedUserId = 2;   
+
+            // Act
+            var follow = new Follow(followerUserId, followedUserId);
+
+            // Assert
+            Assert.Equal(followerUserId, follow.FollowerId);
+            Assert.Equal(followedUserId, follow.FolloweeId);
+            Assert.True((DateTime.UtcNow - follow.CreatedAt).TotalSeconds < 5);
+        }
+    }
+}
