c9c0b58970bc4de35ad54b7b9e1364d78c7441ff
MMTTSEC <mohammed.thabit.syne24lin@edu.tucsweden.se>
2025-11-28 21:34:58 +0100

    test for sender = receiver

---
+++ backend/MyFeed.Domain/Entities/DM.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFeed.Domain.Entities
+{
+    public class DM
+    {
+        public int SenderUserId { get; }
+        public int ReceiverUserId { get; }
+        public string Message { get; }
+        public DateTime CreatedAt { get; }
+        public DM(int senderUserId, int receiverUserId, string message)
+        {
+            SenderUserId = senderUserId;
+            ReceiverUserId = receiverUserId;
+            Message = message;
+            CreatedAt = DateTime.UtcNow;
+            
+            if (senderUserId == receiverUserId)
+            {
+                throw new ArgumentException("A user cannot send a DM to themselves.");
+            }
+        }
+
+    }
+}
---
+++ backend/MyFeed.Tests/Domain/DMTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using MyFeed.Domain.Entities;
+
+namespace MyFeed.Tests.Domain
+{
+    public class DMTests
+    {
+        [Fact]
+        public void CreatingDM_WithSameSenderAndReceiver_ThrowsException()
+        {
+            int userId = 1;
+            string message = "Hello";
+
+            Assert.Throws<ArgumentException>(() => new DM(userId, userId, message));
+        }
+        
+    }
+}
