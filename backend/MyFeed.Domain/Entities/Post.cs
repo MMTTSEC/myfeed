9e5f2d39e5573fd8e020ae8542fb101a3cce62a0
MMTTSEC <mohammed.thabit.syne24lin@edu.tucsweden.se>
2025-11-28 18:05:34 +0100

    test for author = receiver

---
+++ backend/MyFeed.Domain/Entities/Post.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFeed.Domain.Entities
+{
+    public class Post
+    {
+        public int AuthorUserId { get; }
+        public int ReceiverUserId { get; }
+        public string Title { get; }
+        public string Body { get; }
+        public DateTime CreatedAt { get; }
+        public Post(int authorUserId, int receiverUserId, string title, string body)
+        {
+            if (authorUserId == receiverUserId)
+            {
+                throw new ArgumentException("Author and receiver cannot be the same user.");
+            }
+            AuthorUserId = authorUserId;
+            ReceiverUserId = receiverUserId;
+            Title = title;
+            Body = body;
+            CreatedAt = DateTime.UtcNow;
+        }
+
+    }
+}
---
+++ backend/MyFeed.Tests/Domain/PostTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xunit;
+using MyFeed.Domain.Entities;
+
+namespace MyFeed.Tests.Domain
+{
+    public class PostTests
+    {
+        [Fact]
+        public void CreatingPost_WithAuthorEqualsReceiver_ThrowsException()
+        {
+            Assert.Throws<ArgumentException>(() =>
+                new Post(authorUserId: 1, receiverUserId: 1, "Hello", "Body")
+            );
+        }   
+
+    }
+}
