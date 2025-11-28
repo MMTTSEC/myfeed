77bdd11ea234820647fd68c0289b06e1534d5baf
MMTTSEC <mohammed.thabit.syne24lin@edu.tucsweden.se>
2025-11-28 21:13:59 +0100

    create like entity

---
+++ backend/MyFeed.Domain/Entities/Like.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFeed.Domain.Entities
+{
+    public class Like
+    {   
+        public int UserId { get; }
+        public int PostId { get; }
+        public DateTime CreatedAt { get; }
+        public Like(int userId, int postId)
+        {
+            UserId = userId;
+            PostId = postId;
+            CreatedAt = DateTime.UtcNow;
+        }
+    }
+}
---
+++ backend/MyFeed.Tests/Domain/LikeTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using MyFeed.Domain.Entities;
+
+namespace MyFeed.Tests.Domain
+{
+    public class LikeTests
+    {
+        [Fact]
+        public void CreatingLike_withValidData_Succeeds()
+        {
+            var like = new Like(userId: 1, postId: 2);
+            
+            Assert.Equal(1, like.UserId);
+            Assert.Equal(2, like.PostId);
+            Assert.True((DateTime.UtcNow - like.CreatedAt).TotalSeconds < 5);
+
+
+        }
+
+    }
+}
