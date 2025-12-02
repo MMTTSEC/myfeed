ffc0711b53ec893717ddf409bab8d4e819d37e38
MMTTSEC <mohammed.thabit.syne24lin@edu.tucsweden.se>
2025-12-02 11:07:21 +0100

    add IUserRepository

---
+++ backend/MyFeed.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyFeed.Domain.Entities;
+namespace MyFeed.Domain.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByIdAsync(int id);
+        Task<User?> GetByUsernameAsync(string username);
+        Task AddAsync(User user);
+        Task<bool> ExistsAsync(int id);
+
+        
+    }
+}
