5c2a873830820fa0b65b3319a2643bbe1904672a
Pontus Flodström <pontaflod95@hotmail.com>
2025-12-03 11:57:38 +0100

    Added some minor fixes for  NotFoundPage.tsx

--- frontend/src/pages/general-pages/NotFoundPage.tsx
+++ frontend/src/pages/general-pages/NotFoundPage.tsx
@@ -8,12 +8,12 @@ export default function NotFoundPage() {
   return <>
     <section className="center-column NotFoundPage">
       <div className="main-container">
-        <h2>Not Found: 404</h2>
+        <h1>Not Found: 404</h1>
         <p>
           We are sorry, but there doesn't seem to be any page on this
           site that matches the url:
         </p>
-        <p><strong><i>{useLocation().pathname.slice(1)}</i></strong></p>
+        <h3><strong><i>{useLocation().pathname.slice(1)}</i></strong></h3>
         <p>Please <Link to="/home">visit the start page</Link> instead.</p>
       </div>
     </section>
