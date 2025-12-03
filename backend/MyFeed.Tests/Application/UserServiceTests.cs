a672af9b37914b0faf3ff2507a9cb5c1e6d7495d
MMTTSEC <mohammed.thabit.syne24lin@edu.tucsweden.se>
2025-12-03 17:14:43 +0100

    Add unit tests for UserService registration method

---
+++ backend/MyFeed.Tests/Application/UserServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using Moq;
+using MyFeed.Application.Services;
+using MyFeed.Domain.Entities;
+using MyFeed.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MyFeed.Tests.Application
+{
+    public class UserServiceTests
+    {
+        [Fact]
+        public async Task RegisterUser_WithValidData_CallsRepoitory()
+        {
+            var userRepo = new Mock<IUserRepository>();
+            userRepo.Setup(x => x.GetByUsernameAsync("testuser")).ReturnsAsync((User?)null);
+            userRepo.Setup(x => x.AddAsync(It.IsAny<User>()));
+
+            var svc = new UserService(userRepo.Object);
+
+            await svc.RegisterUserAsync("testuser", "password123");
+
+            userRepo.Verify(x => x.AddAsync(It.IsAny<User>()), Times.Once);
+        }
+    }
+}
