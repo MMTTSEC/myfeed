5f3b04c00b16d8911f057ff983fd1dd803a5a934
Pontus Flodström <pontaflod95@hotmail.com>
2025-12-04 12:23:02 +0100

    Created initial WritePost.tsx component.

---
+++ frontend/src/components/WritePost.tsx
@@ -0,0 +1,11 @@
+import '../styles/writepost.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+export default function WritePost() {
+
+  return <>
+    <div className="write-post-container">
+      
+    </div>
+  </>;
+}
