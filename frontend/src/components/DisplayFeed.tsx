3d181451925702f99c486b28290aabbbc45e9dad
Pontus Flodström <pontaflod95@hotmail.com>
2025-12-04 15:22:42 +0100

    Created function currentFeedToDisplay() which takes in the route path and return which feed to display (allPosts, followedPosts, or userPosts) which is stored in the const typeOfFeed

--- frontend/src/components/DisplayFeed.tsx
+++ frontend/src/components/DisplayFeed.tsx
@@ -5,7 +5,23 @@ interface NavigationProps {
   currentPath: string;
 }
 
+function currentFeedToDisplay({ currentPath }: NavigationProps) {
+  switch (currentPath) {
+    case '/home':
+      return 'allPosts';
+    case '/feed':
+      return 'followedPosts';
+    default:
+      if (currentPath.startsWith('/profile/')) {
+        return 'userPosts';
+      }
+      return '';
+  }
+}
+
 export default function DisplayFeed({ currentPath }: NavigationProps) {
+  const typeOfFeed = currentFeedToDisplay({ currentPath });
+
   return <>
   
   </>
