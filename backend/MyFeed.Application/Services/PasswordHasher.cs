4b41364be171c6882492a8765e704e66bb967ccc
MMTTSEC <mohammed.thabit.syne24lin@edu.tucsweden.se>
2025-12-05 01:36:24 +0100

    Add PasswordHasher service

---
+++ backend/MyFeed.Application/Services/PasswordHasher.cs
@@ -0,0 +1,16 @@
+namespace MyFeed.Application.Services
+{
+    public class PasswordHasher
+    {
+        public string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public bool VerifyPassword(string password, string passwordHash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, passwordHash);
+        }
+    }
+}
+
