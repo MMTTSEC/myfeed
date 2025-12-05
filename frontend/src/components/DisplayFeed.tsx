87bc27b33f1df35688456e6598f955062b147130
Pontus Flodström <pontaflod95@hotmail.com>
2025-12-05 11:14:44 +0100

    Added some more placeholder posts for each feed and made the letter in the post avatar uppercased.

--- frontend/src/components/DisplayFeed.tsx
+++ frontend/src/components/DisplayFeed.tsx
@@ -18,13 +18,57 @@ function fetchPostsOfType(type: string): Post[] {
           id: '1',
           author: 'Zenty',
           content: 'This is a post',
+          createdAt: '2025-12-04',
+        },
+        {
+          id: '2',
+          author: 'MMTTSEC',
+          content: 'This is also a post',
+          createdAt: '2025-12-05',
+        },
+        {
+          id: '3',
+          author: 'mycookie5',
+          content: 'Is this a post?',
+          createdAt: '2025-12-05',
+        },
+        {
+          id: '4',
+          author: 'Zenty',
+          content: 'Even this is a post',
           createdAt: '2025-12-05',
         },
       ];
     case 'followedPosts':
-      return [];
+      return [
+        {
+          id: '2',
+          author: 'MMTTSEC',
+          content: 'This is also a post',
+          createdAt: '2025-12-05',
+        },
+        {
+          id: '3',
+          author: 'mycookie5',
+          content: 'Is this a post?',
+          createdAt: '2025-12-05',
+        },
+      ];
     case 'userPosts':
-      return [];
+      return [
+        {
+          id: '1',
+          author: 'Zenty',
+          content: 'This is a post',
+          createdAt: '2025-12-04',
+        },
+        {
+          id: '3',
+          author: 'Zenty',
+          content: 'Even this is a post',
+          createdAt: '2025-12-05',
+        },
+      ];
     default:
       return [];
   }
@@ -36,7 +80,7 @@ function PostCard({ post }: { post: Post }) {
 
       <div className="post-header">
         <figure className="post-author-avatar">
-          <strong>{post.author.charAt(0)}</strong>
+          <strong>{post.author.charAt(0).toUpperCase()}</strong>
         </figure>
         <strong>{post.author}</strong>
         <span className="post-date">{post.createdAt}</span>
