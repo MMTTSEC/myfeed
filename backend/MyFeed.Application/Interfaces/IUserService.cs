4091215e2b29a7d7299f285c42900e180d08ba63
MMTTSEC <mohammed.thabit.syne24lin@edu.tucsweden.se>
2025-12-05 01:37:08 +0100

    Refactor IUserService and UserService to update user registration and add login functionality with password hashing

--- backend/MyFeed.Application/Interfaces/IUserService.cs
+++ backend/MyFeed.Application/Interfaces/IUserService.cs
@@ -5,7 +5,8 @@ namespace MyFeed.Application.Interfaces
 {
     public interface IUserService
     {
-        Task RegisterUserAsync(string username, string passwordHash);
+        Task RegisterUserAsync(string username, string password);
+        Task<User?> LoginAsync(string username, string password);
         Task<User?> GetUserByIdAsync(int id);
         Task<User?> GetUserByUsernameAsync(string username);
     }
