1d4ed07428c66630a222b1850a91cc5ddc11cda5
Pontus Flodström <pontaflod95@hotmail.com>
2025-12-05 14:04:09 +0100

    Finished the profile information + profile action buttons, aswell as their design.

--- frontend/src/components/DisplayProfileHeader.tsx
+++ frontend/src/components/DisplayProfileHeader.tsx
@@ -5,7 +5,32 @@ export default function DisplayProfileHeader() {
 
   return (
     <div className="profile-header">
-      Content that only shows on profile pages
+      <div className="profile-header-left">
+        <h1 className='profile-name'><span className="profile-name-at">@</span>Zenty</h1>
+        <span className="profile-creation-date"><i className="bi bi-calendar"></i>Joined November 2025</span>
+      </div>
+      <div className="profile-header-right">
+        <button className="profile-action button-clear-messages">
+          <i className="bi bi-exclamation-circle"></i>
+          Clear Messages
+        </button>
+        <button className="profile-action button-delete-account">
+          <i className="bi bi-exclamation-triangle"></i>
+          Delete Account
+        </button>
+        <button className="profile-action button-message">
+          <i className="bi bi-chat"></i>
+          Message
+        </button>
+        <button className="profile-action button-follow">
+          <i className="bi bi-person-plus"></i>
+          Follow
+        </button>
+        <button className="profile-action button-unfollow">
+          <i className="bi bi-person-dash"></i>
+          Unfollow
+        </button>
+      </div>
     </div>
   );
 }
