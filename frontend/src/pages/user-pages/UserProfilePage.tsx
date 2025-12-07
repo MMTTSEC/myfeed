554bb13abc39eef74875999eb70beeac5d075e3d
MMTTSEC <mohammed.thabit.syne24lin@edu.tucsweden.se>
2025-12-07 23:13:55 +0100

    Add ProtectedRoute component and integrate it into pages

--- frontend/src/pages/user-pages/UserProfilePage.tsx
+++ frontend/src/pages/user-pages/UserProfilePage.tsx
@@ -3,25 +3,28 @@ import Footer from '../../components/Footer';
 import HandlePageHeader from '../../components/HandlePageHeader';
 import DisplayFeed from '../../components/DisplayFeed';
 import DisplayFollowing from '../../components/DisplayFollowing';
+import ProtectedRoute from '../../components/ProtectedRoute';
 
 UserProfilePage.route = {
   path: '/profile/'
 };
 
 export default function UserProfilePage() {
-  return <>
-    <section className="left-column UserProfilePage">
-      <Navigation currentPath={UserProfilePage.route.path} />
-      <Footer />
-    </section>
-    <section className="center-column UserProfilePage">
-      <div className="main-container">
-        <HandlePageHeader currentPath={UserProfilePage.route.path} />
-        <DisplayFeed currentPath={UserProfilePage.route.path} />
-      </div>
-    </section>
-    <section className="right-column UserProfilePage">
-      <DisplayFollowing />
-    </section>
-  </>;
+  return (
+    <ProtectedRoute>
+      <section className="left-column UserProfilePage">
+        <Navigation currentPath={UserProfilePage.route.path} />
+        <Footer />
+      </section>
+      <section className="center-column UserProfilePage">
+        <div className="main-container">
+          <HandlePageHeader currentPath={UserProfilePage.route.path} />
+          <DisplayFeed currentPath={UserProfilePage.route.path} />
+        </div>
+      </section>
+      <section className="right-column UserProfilePage">
+        <DisplayFollowing />
+      </section>
+    </ProtectedRoute>
+  );
 }
